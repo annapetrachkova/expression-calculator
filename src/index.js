30cb910ae5df75e7a3b3c00fd9e74391970557e9
Анна Петрачкова <p.anytk@mail.ru>
2021-08-06 00:08:39 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let open = 0, close = 0;
+    for (const item of expr) {
+        if (item === "(") {
+            open++;
+        }
+        if (item === ")") {
+            close++;
+        }
+    }
+
+    if (expr.includes('/ 0')) {
+        throw new Error("TypeError: Division by zero.");
+    } else if (open !== close) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    } else {
+        return new Function(`return ${expr}`)();
+    }
 }
 
 module.exports = {
